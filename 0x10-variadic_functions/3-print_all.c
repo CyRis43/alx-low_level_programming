e511f0504fe6613ce90fc1647556aac35fade59d
CyRis43 <manganyecharfred@gmail.com>
2023-10-22 09:09:48 -0700

    print

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,22 +8,19 @@
  * @format: list of types of arguments passed to the function
  */
 void print_all(const char * const format, ...)
-	{
-		int i = 0;
-		char *str, *sep = "";
+{
+	int i = 0;
+	char *str, *sep = "";
 
+	va_list list;
 
-		va_list list;
+	va_start(list, format);
 
-
-		va_start(list, format);
-
-
-		if (format)
+	if (format)
 		{
 			while (format[i])
 			{
-				switch (format[i])
+ 				switch (format[i])
 				{
 					case 'c':
 						printf("%s%c", sep, va_arg(list, int));
@@ -52,5 +49,5 @@ void print_all(const char * const format, ...)
 
 		printf("\n");
 		va_end(list);
-	}
+}
 
